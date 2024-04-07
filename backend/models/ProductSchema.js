9a522d0dc08d878d847addf930f5a51395f1ad28
Alistair Abao <abaolester@gmail.com>
2024-04-07 13:02:14 +0200

    product schema created

---
+++ backend/models/ProductSchema.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import categories from "../utils/categoryObject.js";
+const { Schema } = mongoose;
+
+const ProductSchema = new Schema({
+  prodName: {
+    type: String,
+    required: true,
+  },
+  prodQty: {
+    type: Number,
+    required: true,
+  },
+  prodCategory: {
+    type: String,
+    enum: Object.values(categories),
+    required: true,
+  },
+});
+
+export const ProductModel = mongoose.model("ProductModel", ProductSchema);
