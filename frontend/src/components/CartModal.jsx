c59e39f3db90306662135f6005b1d4bcfa0bc5d7
Alistair Abao <abaolester@gmail.com>
2024-04-25 09:41:27 +0200

    display cart items with total

--- frontend/src/components/CartModal.jsx
+++ frontend/src/components/CartModal.jsx
@@ -3,16 +3,35 @@
 import { Button, Modal } from "flowbite-react";
 import { useState } from "react";
 import axios from "axios";
-import { toast } from "react-toastify";
-import { FaBeer } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 
 export function CartModal(user) {
   const [openModal, setOpenModal] = useState(false);
+
   //user data as context passed as props from NavbarComponent
   const userData = user.user.data.user;
-  //   console.log(userData);
+  console.log(userData.cart);
 
+  //function to obtain total price
+  const totalPrice = () => {
+    const priceArray = [];
+    userData.cart.map((cartProducts) => {
+      priceArray.push(cartProducts.productId.price);
+    });
+    const initialValue = 0;
+    const totalCartPrice = priceArray.reduce(
+      (accum, current) => accum + current,
+      initialValue
+    );
+    return totalCartPrice;
+  };
+
+  //convert number to price in euros
+  let euro = Intl.NumberFormat("en-DE", {
+    style: "currency",
+    currency: "EUR",
+  });
+  // jsx
   return (
     <main>
       <Button onClick={() => setOpenModal(true)}>
@@ -35,16 +54,23 @@ export function CartModal(user) {
               consumer privacy laws for its citizens, companies around the world
               are updating their terms of service agreements to comply.
             </p>
+
+            {/* map cart array to display all product details */}
             {userData.cart.map((newCart) => {
-              console.log(newCart);
+              //   console.log(newCart);
               return (
                 <section key={newCart._id}>
-                  <p className='text-base leading-relaxed text-gray-500 dark:text-gray-400'>
-                    {newCart.prodName}
-                  </p>
+                  <article className='text-base leading-relaxed text-gray-500 dark:text-gray-400'>
+                    {newCart.productId.prodName}
+                  </article>
+                  <article>€{newCart.productId.price}</article>
                 </section>
               );
             })}
+            {/* Use currency converter on the obtained total price */}
+            <section className='text-right'>
+              Total Price: {euro.format(totalPrice())}
+            </section>
           </div>
         </Modal.Body>
         <Modal.Footer>
